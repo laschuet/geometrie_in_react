e177df4c01b61e1c06920f1effbe8a7306c52818
laschuet <laschuet@users.noreply.github.com>
2017-04-18 10:40:41 +0200

    Implement cuboid's volume

--- src/components/Cuboid.js
+++ src/components/Cuboid.js
@@ -2,32 +2,87 @@ import React from 'react';
 
 import styles from 'stylesheets/components/common/typography';
 
-const Cuboid = () => (
-  <div>
-    <h1 className={styles.heading}>Quader</h1>
-    <div>
-      <h2>Eingabe</h2>
-      <form>
+class Cuboid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      edgeA: 0,
+      edgeB: 0,
+      edgeC: 0,
+      volume: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    let edgeA = this.state.edgeA;
+    let edgeB = this.state.edgeB;
+    let edgeC = this.state.edgeC;
+    if (name === 'edgeA') {
+      edgeA = value;
+    } else if (name === 'edgeB') {
+      edgeB = value;
+    } else {
+      edgeC = value;
+    }
+    const volume = this.computeVolume(edgeA, edgeB, edgeC);
+
+    this.setState({
+      [name]: value,
+      volume: volume
+    });
+  }
+
+  computeVolume(edgeA, edgeB, edgeC) {
+    return edgeA * edgeB * edgeC;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className={styles.heading}>Quader</h1>
         <div>
-          <label id="quader_a">a:
-            <input type="text" />
-          </label>
+          <h2>Eingabe</h2>
+          <form>
+            <div>
+              <label>Seite a:
+                <input
+                  type="text"
+                  name="edgeA"
+                  value={this.state.edgeA}
+                  onChange={this.handleChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>Seite b:
+                <input
+                  type="text"
+                  name="edgeB"
+                  value={this.state.edgeB}
+                  onChange={this.handleChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>Seite c:
+                <input
+                  type="text"
+                  name="edgeC"
+                  value={this.state.edgeC}
+                  onChange={this.handleChange}
+                />
+              </label>
+            </div>
+          </form>
+          <h2>Volumen</h2>
+          <span>{this.state.volume}</span>
         </div>
-        <div>
-          <label id="quader_b">b:
-            <input type="text" />
-          </label>
-        </div>
-        <div>
-          <label id="quader_c">c:
-            <input type="text" />
-          </label>
-        </div>
-      </form>
-      <h2>Volumen</h2>
-      <span>0</span>
-    </div>
-  </div>
-);
+      </div>
+    )
+  }
+}
 
 export default Cuboid;
